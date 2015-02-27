ccbfa8ffb569de0e99cc6bed4bde165e3e700800
Michael Kempf <88schadenfreude@gmail.com>
2015-02-26 16:37:09 -0800

    renders correctly

--- board.rb
+++ board.rb
@@ -1,42 +1,84 @@
+require 'colorize'
+require_relative 'piece'
+
 class Board
   attr_reader :grid
-  def initialize
-    @grid = Array.new(8) { Array.new(8, nil) }
+
+
+  def initialize(pop = true)
+    @grid = Array.new(8) { Array.new(8){nil} }
+    generate_board if pop
   end
 
-  def populate_grid
-    #iterate over grid and fill with pieces
-  end
-
-  def occupied?(target)
-
-  end
-
-  def on_board?(target)
-    #use rubies between? method
-  end
-
-  def render
-    print " "
-    (0..7).each do |num| print "  #{num}" end
-    puts ""
-    n = 0
-
-    grid.each do |row|
-      print "#{n} "
-      row.each do |piece|
-        if piece.nil?
-          print " - "
-        else
-          print " #{piece.inspect} "
-        end
+  # Fills the board with b on top w on bottom
+  def generate_board
+    [0, 1, 2].each do |row|
+      0.upto(7) do |col|
+        offset = row + col
+        self[[row, col]] = Piece.new([row, col], self, :B) if offset.odd?
       end
-      n += 1
-      puts "\n"
+    end
+
+    [5, 6, 7].each do |row|
+      0.upto(7) do |col|
+        offset = row + col
+        self[[row, col]] = Piece.new([row, col], self, :W) if offset.odd?
+      end
     end
   end
 
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    grid[row][col] = value
+  end
+
+  def empty?(pos)
+    self[pos] == nil
+  end
+
+  def deep_dup
+    new_board = Board.new(false)
+
+    grid.flatten.compact.each do |piece|
+      new_board[piece.pos.dup] =
+          Piece.new(piece.pos.dup, new_board, piece.color)
+    end
+
+    new_board
+  end
   def display
-    render
+    puts render
+  end
+
+  def render
+    display_str = "  0 1 2 3 4 5 6 7 \n"
+    @grid.each_with_index do |row, i|
+      display_str += "#{i.to_s} "
+      row.each_with_index do |tile, index|
+        if i.even? && index.even? || index.odd? && i.odd?
+          color_tile = :light_white
+        else
+          color_tile = :pink
+        end
+          if tile.nil?
+            display_str += "  ".colorize(:background => color_tile)
+          else
+            display_str +="#{tile.symbol} ".colorize(:background => color_tile)
+          end
+        end
+      display_str += "\n"
+    end
+    display_str
   end
 end
+
+b = Board.new
+b.display
+
+# I am so hungry!!!
+# no I'm not
--- piece.rb
+++ piece.rb
@@ -1,53 +1,92 @@
-class Pieces
-  attr_accessor :pos, :king
+class Piece
+  attr_accessor :pos, :king, :symbol
+  attr_reader :board, :color
 
   #first num is row second is column
-  #black is always at top of board & red at bottom
-  BLACK_MOVE_DIRS = [
+  #black is always at top of board & white at bottom
+  BLACK_DIRS = [
     [1, -1],
     [1, 1]
   ]
-  RED_MOVE_DIRS = [
+  WHITE_DIRS = [
     [-1, -1],
     [-1, 1]
   ]
+  PIECE_UNICODE = {
+    B: "\u25CF",
+    W: "\u25CF"
+    }
+  KING_UNICODE  = {
+    B: "\u25C9",
+    W: "\u25C9"
+    }
 
-  def initialize(color, pos, board)
-    @color, @pos, @board = color, pos, board
+  def initialize(pos, board, color)
+    @pos, @board, @color = pos, board, color
     @king = false
+    @symbol = '-'
   end
 
-  def perform_slide(target_pos)
-    #if worked return true else false
-
+  def perform_slide(end_pos)
+    if board.empty?(end_pos) && move_diffs.include?(end_pos)
+      board[end_pos] = self
+      board[pos] = nil
+      @pos = end_pos
+      maybe_promote
+      true
+    else
+      false
+    end
   end
 
   def perform_jump(target_pos)
     #if worked return true else false
   end
 
-  def legal_move?(target_pos)
-    #self.pos
-    #is target empty?
-    #is occupied by other color?
-    #occupied by same color?
+  def move_diffs
+    if king
+      poss_moves(BLACK_DIRS + WHITE_DIRS)
+    elsif :B
+      poss_moves(WHITE_DIRS)
+    else
+      poss_move(BLACK_DIRS)
+    end
   end
 
-  def move_diffs(dirs)
-    potential_moves = []
-    #self.pos
-    #itterate through dirs add each dir to current piece pos
-    #push each new pos into potential moves
-    #return potential moves
+  def poss_moves(dirs)
+    possible_moves = []
+    dirs.each do |row, col|
+      new_pos = [pos[0] + row, pos[1] + col]
+      if board.empty?(new_pos) || board[new_pos].color != color
+        possible_moves << new_pos
+      end
+    end
+
+    possible_moves
   end
 
   def maybe_promote
-    #checks to see if new pos is now on opposite side of the board
+    if color == :B
+      self.king = true if back_row?
+    else
+      self.king = true if back_row?
+    end
+  end
+
+  def end_row?
+    if color == :B
+      pos[0] == 7
+    else
+      pos[0] == 0
+    end
   end
 
   def perform_moves(move_sequence)
-    #takes pos and checks for valid moves
-    #at each valid move again checks for valid from new pos
-    #until no more valid moves
+
+  end
+
+  def symbol
+    unicode = king ? KING_UNICODE[color] : PIECE_UNICODE[color]
+    unicode.colorize(color)
   end
 end
